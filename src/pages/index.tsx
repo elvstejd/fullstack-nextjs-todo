6014449b7862e20b0451504015eafba09491b5ae
elvstejd <elvistejeda0910@gmail.com>
2022-09-10 10:10:22 -0400

    fix: add user box component with grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,30 @@ import type { NextPage } from "next";
 import { useTasksQuery } from "../hooks/useTasksQuery";
 import { Task } from "../components/Task";
 import { TaskInput } from "../components/TaskInput";
+import { UserBox } from "../components/UserBox";
 
 const Home: NextPage = () => {
   const { data: tasks } = useTasksQuery();
 
   return (
     <>
-      <Box
-        sx={{
-          maxWidth: "30rem",
-          margin: "0 auto",
-          marginTop: "3rem",
-          marginBottom: "4.5rem",
-        }}
-      >
-        <Title mb={16}>My Todos</Title>
-        {tasks.map((task) => (
-          <Task key={task.id} data={task} />
-        ))}
+      <Box sx={{ display: "grid", gridTemplateColumns: "1fr 2fr" }}>
+        <UserBox />
+        <Box
+          sx={{
+            maxWidth: "30rem",
+            margin: "0 auto",
+            marginTop: "3rem",
+            marginBottom: "4.5rem",
+          }}
+        >
+          <Title mb={16}>My Todos</Title>
+          {tasks.map((task) => (
+            <Task key={task.id} data={task} />
+          ))}
+        </Box>
+        <TaskInput />
       </Box>
-      <TaskInput />
     </>
   );
 };
