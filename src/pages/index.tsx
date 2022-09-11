a35e482fd353a90e03fda1d1ab44ba4d7cea53bb
elvstejd <elvistejeda0910@gmail.com>
2022-09-11 14:50:46 -0400

    fix: add auth prompt when not signed in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
-import { Box, Paper } from "@mantine/core";
+import { Alert, Box, Paper } from "@mantine/core";
 import type { NextPage } from "next";
 import { useTasksQuery } from "../hooks/useTasksQuery";
 import { Task } from "../components/Task";
 import { TaskInput } from "../components/TaskInput";
 import { Header } from "../components/Header";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   const { data: tasks } = useTasksQuery();
+  const { data: session } = useSession();
 
   return (
     <>
@@ -20,6 +22,13 @@ const Home: NextPage = () => {
             marginBottom: "4.5rem",
           }}
         >
+          {!session && (
+            <Alert color="yellow" mb={16}>
+              Necesitarás una cuenta para crear tareas. Puedes continuar creando
+              un perfil temporal o bien creando una cuenta de usuario.
+            </Alert>
+          )}
+
           <Paper withBorder shadow="xs">
             {tasks.map((task, index) => (
               <Task index={index} key={task.id} data={task} />
